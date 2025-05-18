2ce1d50f7b2d4ff684ebe6740512c3110000de01
ryuta-hiruma <42663222+ryuta-hiruma@users.noreply.github.com>
2025-05-18 21:02:52 +0900

    first commit

---
+++ src/main/java/com/example/event_reservation_app/EventReservationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.event_reservation_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventReservationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventReservationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/event_reservation_app/EventReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.event_reservation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventReservationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
